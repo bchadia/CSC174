6cce88452a1fee8401df016bc9ef9114d385b374
chadia <chadiabedetse@gmail.com>
2019-02-27 15:58:30 -0500

    changed fonts and color fonts

--- assignment3/index.php
+++ assignment3/index.php
@@ -1,48 +1,42 @@
 <!DOCTYPE html>
 <html>
-
 <head>
-   <title>Home| Assignment 3</title>
+   <title> Skittles </title>
    <link rel="stylesheet" href="css/styles.css">
-   <link href="https://fonts.googleapis.com/css?family=Amatic+SC" rel="stylesheet">
-      <link href="https://fonts.googleapis.com/css?family=Open+Sans:300i" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Montserrat|Roboto" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Amatic+SC" rel="stylesheet">
       <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300" rel="stylesheet">
       <link href="https://fonts.googleapis.com/css?family=Anton" rel="stylesheet">
-      <link href="https://fonts.googleapis.com/css?family=Roboto:300i" rel="stylesheet">
 </head>
-
-
 <body class = container>
    <header>
-      <a href = "index.php">
-         <h1> Three Unrelated Related Pages</h1>
-      </a>
+        <h1>Skittles</h1>
    </header>
 
-   <div class = headImage>
-      <img src ="images/nike.jpg" alt="Nike Huraches">
-      <p>This page is about the history and different types of shoes. Ranging from sneakers, sandals, and boots. The page describes the differences between the shoes and their similarities in terms of construction and parts.  </p>
-      <img src ="images/skittles.jpeg" alt="Skittles">
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-      proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      <img src ="images/question.png" alt="Picture Here">
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-      proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-   </div>
+   <main>
+       <h2 class="intro">Intro</h2>
+       <p>Skittles candy products, produced by the Wm. Wrigley Jr. Company, come in a wide variety. Most of the varieties are available only in particular regions of the world. The oldest Skittles product is Original Fruit Skittles, which was originally released in Europe in 1974, and was launched in the United States in 1979.[1] The most recent flavor, Sweet Heat, was released in 2018. Earlier Skittles products tended to focus on fruit flavors, more recent products have branched out to include flavors such as chocolate, bubble gum, popcorn, mint, and sours. Skittles products also come in a wide variety of colors, although different Skittles products often use the same colors for sweets with different flavors. For example, the color yellow is associated with lemon flavor in Original Fruit Skittles and as banana berry in Tropical Skittles.</p>
 
+      <section>
+       <h2 class="strawberry">Garden Strawberry</h2>
+       <p>The garden strawberry (or simply strawberry; Fragaria × ananassa)[1] is a widely grown hybrid species of the genus Fragaria, collectively known as the strawberries. It is cultivated worldwide for its fruit. The fruit is widely appreciated for its characteristic aroma, bright red color, juicy texture, and sweetness. It is consumed in large quantities, either fresh or in such prepared foods as preserves, juice, pies, ice creams, milkshakes, and chocolates. Artificial strawberry flavorings and aromas are also widely used in many products like lip gloss, candy, hand sanitizers, perfume, and many others. <a href="https://en.wikipedia.org/wiki/Strawberry"> Learn more about Garden Strawberry</a></p>
 
-   <?php include "inc/footer.inc";?>
+       <h2 class="lemon">Lemon</h2>
+       <p>The lemon, Citrus limon (L.) Osbeck, is a species of small evergreen tree in the flowering plant family Rutaceae, native to South Asia, primarily North eastern India. The tree's ellipsoidal yellow fruit is used for culinary and non-culinary purposes throughout the world, primarily for its juice, which has both culinary and cleaning uses.[2] The pulp and rind (zest) are also used in cooking and baking. The juice of the lemon is about 5% to 6% citric acid, with a pH of around 2.2, giving it a sour taste. The distinctive sour taste of lemon juice makes it a key ingredient in drinks and foods such as lemonade and lemon meringue pie. <a href="https://en.wikipedia.org/wiki/Lemon">Learn more about Lemon</a></p>
 
-   <?php include "inc/nav.inc";?>
+       <h2 class="apple">Green Apple</h2>
+       <p>An apple is a sweet, edible fruit produced by an apple tree (Malus pumila). Apple trees are cultivated worldwide and are the most widely grown species in the genus Malus. The tree originated in Central Asia, where its wild ancestor, Malus sieversii, is still found today. Apples have been grown for thousands of years in Asia and Europe and were brought to North America by European colonists. Apples have religious and mythological significance in many cultures, including Norse, Greek and European Christian traditions.<a href="https://en.wikipedia.org/wiki/Apple"> Learn more about Green Apple</a> </p>
+    </section>
+    
+   </main>
 
+<footer>
+      CSC 174: Advanced Front-end Web Design and Development
+</footer>
+
+<nav>
+  <a href="http://csc174.org/assignment03/rochester/index.php"> Original Website </a>
+</nav>
 
    <script src="http://code.jquery.com/jquery.js"></script>
    <script src="js/menu-highlighter.js"></script>
